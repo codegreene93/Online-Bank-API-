f6421153f670871104771fb7124104ded77a8421
codegreene93 <34109141+codegreene93@users.noreply.github.com>
2020-04-10 18:26:44 +0100

    Second Attempt

--- OnlineBank/src/main/java/databases/DatabaseCustomer.java
+++ OnlineBank/src/main/java/databases/DatabaseCustomer.java
@@ -5,8 +5,8 @@ import java.util.HashMap;
 public class DatabaseCustomer {
 
 	private static HashMap <Integer, String> customer = new HashMap<Integer, String>();
-	private static HashMap <Integer, Integer> account = new HashMap<Integer, Integer>();
-	private static HashMap <Integer, Integer> transaction = new HashMap<Integer, Integer>();
+	private static HashMap <Integer, String> account = new HashMap<Integer, String>();
+	private static HashMap <String, Integer> transaction = new HashMap<String, Integer>();
 	
 	private static HashMap <Integer, String> getCustomer(){
 		
@@ -14,14 +14,14 @@ public class DatabaseCustomer {
 		
 	} 
 	
-	private static HashMap <Integer, Integer> getAccount(){
+	private static HashMap <Integer, String> getAccount(){
 		
 		return account;
 	} 
 	
-	private static HashMap <Integer, Integer> getTransaction(){
+	private static  HashMap <String, Integer> getTransaction(){
 		
-		return account;
+		return transaction;
 		
 	}
 }
