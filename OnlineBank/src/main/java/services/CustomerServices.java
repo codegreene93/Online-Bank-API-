08f58fe1686cd4ff126ff26b242e35d7795a941c
Wilmir <wbnicanor01@gmail.com>
2020-04-18 08:12:49 +0100

    Updates the addition of data to the map

--- OnlineBank/src/main/java/services/CustomerServices.java
+++ OnlineBank/src/main/java/services/CustomerServices.java
@@ -18,11 +18,20 @@ public class CustomerServices {
 	Customer cust2 = new Customer(002, "Julian", "Drumcondra, Dublin", "Julian@test.com", "hAppyda$h","hAppyda$h", 26);
 	Customer cust3 = new Customer(003, "Rachel", "Kevin Street Lower, Dublin", "Rachel@test.com", "Password1", "Password1", 27);
 	
+	customers.put(cust1);
+	customers.put(cust2);
+	customers.put(cust3);
+
+		
+	/*
+	// This is a duplicate of above.
 	customers.put(001, new Customer(1, "Reggie", "Eyre Square, Galway,", "test@test.com", "FunkyMonk3y!","FunkyMonk3y!", 25));
 	customers.put(002, new Customer(2,"Julian", "Drumcondra, Dublin", "Julian@test.com", "hAppyda$h","hAppyda$h", 26 ));
 	customers.put(003, new Customer(3,"Rachel", "Kevin Street Lower, Dublin", "Rachel@test.com", "Password1", "Password1", 27));
 	
 	Map<Integer, Account> customer1 = customers.get(001).getAccounts();
+	*/
+		
 	}
 	
 	public List<Customer> getCustomers(){
@@ -44,8 +53,7 @@ public class CustomerServices {
 		return customer;
 	}
 	
-	
-	
-	
+	//1. Add a delete customer method here
+	//2. Add a getCustomer by name and password (you can use either PathParam or QueryParam	
 	
 }
